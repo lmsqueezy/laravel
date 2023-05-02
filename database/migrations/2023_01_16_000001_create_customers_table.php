2c73f74502a214bb2ca3450ced5e0a46216d9db5
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2023-05-02 22:33:10 +0600

    chore: convert to anonymous class migration

--- database/migrations/2023_01_16_000001_create_customers_table.php
+++ database/migrations/2023_01_16_000001_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
+return new class extends Migration
 {
     public function up(): void
     {
@@ -19,4 +19,4 @@ class CreateCustomersTable extends Migration
             $table->index(['billable_id', 'billable_type']);
         });
     }
-}
+};
