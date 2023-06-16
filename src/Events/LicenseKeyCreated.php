7ccf57816d9eb63afa2437133ac8e4da331fd4e8
Dries Vints <dries@vints.be>
2023-06-16 22:42:56 +0200

    Add remaining events

---
+++ src/Events/LicenseKeyCreated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LemonSqueezy\Laravel\Events;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LicenseKeyCreated
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * The billable entity.
+     */
+    public Model $billable;
+
+    /**
+     * The payload array.
+     */
+    public array $payload;
+
+    public function __construct(Model $billable, array $payload)
+    {
+        $this->billable = $billable;
+        $this->payload = $payload;
+    }
+}
