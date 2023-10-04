36c0e87901d123b4d8f7f499fb39b25b700030c5
Dries Vints <dries@vints.be>
2023-10-04 21:13:38 +0200

    Support for customer portals

---
+++ src/Exceptions/InvalidCustomer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LemonSqueezy\Laravel\Exceptions;
+
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+
+class InvalidCustomer extends Exception
+{
+    public static function notYetCreated(Model $owner): static
+    {
+        return new static(class_basename($owner).' is not a Stripe customer yet. See the createAsStripeCustomer method.');
+    }
+}
