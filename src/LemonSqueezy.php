6a6b103cf6eb5de955fee7420ed0d7c184f93ee0
Dries Vints <dries@vints.be>
2023-10-10 11:05:29 +0200

    Update LemonSqueezy.php

--- src/LemonSqueezy.php
+++ src/LemonSqueezy.php
@@ -9,7 +9,7 @@ use LemonSqueezy\Laravel\Exceptions\LemonSqueezyApiError;
 
 class LemonSqueezy
 {
-    const VERSION = '1.2.1';
+    const VERSION = '1.2.2';
 
     /**
      * Indicates if migrations will be run.
