efd1283d9a82de95dbff763313a11772b762f9ff
Rok Vetršek <rok.vetrsek@gmail.com>
2023-12-12 11:17:24 +0100

    fix: Missing Illuminate Builder import in Order model (#68)

--- src/Order.php
+++ src/Order.php
@@ -3,6 +3,7 @@
 namespace LemonSqueezy\Laravel;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
