5157285ed98b403f36c6f1d348c4539f13712c69
Jorge Lapa <2780099+heyjorgedev@users.noreply.github.com>
2024-03-22 09:35:07 +0000

    fix ListenCommandTest missing exception assertion

--- tests/Feature/Commands/ListenCommandTest.php
+++ tests/Feature/Commands/ListenCommandTest.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Validation\ValidationException;
 
 it('can call the listen command', function () {
     config()->set([
@@ -14,5 +15,6 @@ it('can call the listen command', function () {
 });
 
 it('can validate services', function () {
+    $this->expectException(ValidationException::class);
     expect(Artisan::call('lmsqueezy:listen', ['service' => 'invalid']))->toEqual(Command::FAILURE);
 });
